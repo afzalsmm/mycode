01c3c2eadd66472234eaf43939dd8ac8c3c36982
afzalsmm <afzalsmm@users.noreply.github.com>
2021-11-29 17:49:46 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,10 @@
+/* Go Basic challenge
+   Print name, city, state */
+
+   package main
+   import "fmt"
+
+   func main() {
+	   fmt.Println("My name is Afzal")
+	   fmt.Println("I live in Bliane, MN")
+   }
